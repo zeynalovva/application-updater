0edfad1434423e31690f12feb870e151e8b787ae
zeynalovva <zeynalovvabbas@gmail.com>
2025-04-04 01:59:03 +0400

    Updater finished, but still needs maintance

--- BTL/src/main/java/com/zeynalovv/Testing/main.java
+++ BTL/src/main/java/com/zeynalovv/Testing/main.java
@@ -2,8 +2,6 @@ package com.zeynalovv.Testing;
 
 public class main {
     public static void main(String[] args) {
-        String t = "salam";
-        t = new String(t.replaceAll("sa","bf"));
-        System.out.println(t);
+        System.out.println(System.getProperty("user.dir"));
     }
 }
