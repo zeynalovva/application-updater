271f9a35dabe9fb0fe19ea71627262f9d76e8baa
zeynalovva <zeynalovvabbas@gmail.com>
2025-04-23 21:28:04 +0400

    JavaDoc is added (not finished yet)

--- src/main/java/com/zeynalovv/BI4L/Client.java
+++ src/main/java/com/zeynalovv/BI4L/Client.java
@@ -27,6 +27,22 @@ public class Client implements Updatable{
     private Map<String, String> hashTable;
 
 
+    /**
+     * A constructor for the class
+     * @author zeynalovvabbas
+     * @param localPath the path which the original and new version of the files are stored.
+     * @param fileName a custom name for the checksum file.
+     * @param remotePath is the path in which the new release of the application is going to be stored in the server.
+     * @param ipAddress the library uses the SFTP protocol, so the IP address is supplied to <code>Client</code>.
+     * @param port port
+     * @param username username
+     * @param password the port, username, and the password for the SFTP connection.
+     * @param ignoreItems is a <code>Map</code> in which the path is stored as a key, and the type of the path,
+     *                   whether it is a directory or a file is kept as a value which are going to be
+     *                   ignored in the process of updating. See GitHub repo for more.
+     *
+     */
+
     public Client(String localPath, String fileName, String remotePath, String ipAddress,
                   String port, String username, String password, Map<Path, String> ignoreItems){
 
@@ -40,6 +56,22 @@ public class Client implements Updatable{
         this.ignoreItems = ignoreItems;
     }
 
+    /**
+     * Scans the <code>localPath</code>, which refers to the directory containing the new but unreleased
+     * version of the application on the developer's computer. The absolute paths of all files and directories
+     * found are stored in a list called <code>absoluteFile</code>. Then, only the folder paths are selected,
+     * converted to relative paths, and stored in another list called <code>relativeDir</code>.
+     * <p>
+     * <code>hashTable</code> - is a <code>Map</code>, containing hash value(checksum) of each file in the key,
+     * and their relative paths in the value section.
+     *
+     * @throws IOException as parser throws IOException when writing the data into a JSON while, so does <code>start()
+     * </code>
+     * @throws NoSuchAlgorithmException when hashing function receives a wrong hashing algorithm
+     * @throws SftpException when an error occurs in <code>Uploader</code>
+     * @throws JSchException when an error occurs in <code>Uploader</code>
+     */
+
     public void start() throws IOException, NoSuchAlgorithmException, SftpException, JSchException {
         this.absoluteFile = this.scanPath(localPath).files();
         this.relativeDir = relativize(this.scanPath(localPath).directories());
@@ -49,28 +81,19 @@ public class Client implements Updatable{
         for(Path x : absoluteFile){
             hashTable.put(hashOf(x), String.valueOf(localPath.relativize(x)));
         }
-
         parser();
         upload();
-
     }
 
-    public void upload() throws JSchException, SftpException {
-        Uploader uploader = new Uploader(ipAddress, port, username, password, remotePath);
-        uploader.openConnection();
-        for(String i : relativeDir){
-            if(uploader.createFolder(i))
-                System.out.println("Created: " + i);
-        }
-
-        for (Path i : absoluteFile) {
-            String filePath = String.valueOf(i);
-            String serverPath = String.valueOf(remotePath.resolve(localPath.relativize(i)));
-            System.out.println("Uploading: " + filePath + " - to - " + serverPath);
-            uploader.uploadFile(filePath, serverPath);
-        }
-        uploader.close();
-    }
+    /**
+     * Parses the context of <code>hashTable</code> to a JSON file. However, the data is seperated into several
+     * clusters in the JSON file.
+     * <p>
+     * Those which are going to be ignored - <code>ignoreList</code>, files - <code>files</code>,
+     *  directories - <code>folders</code>, converted version of files to percent encoding - <code>translated</code>.
+     *
+     * @throws IOException when an error occurs with the checksum file
+     */
 
     public void parser() throws IOException {
         ObjectMapper json = new ObjectMapper();
@@ -82,7 +105,7 @@ public class Client implements Updatable{
 
         for(String i : hashTable.keySet()){
             files.put(i, hashTable.get(i));
-            translated.put(i, translator(hashTable.get(i).toCharArray()));
+            translated.put(i, URLEncoding(hashTable.get(i).toCharArray()));
         }
 
         for(String i : relativeDir){
@@ -105,6 +128,37 @@ public class Client implements Updatable{
 
     }
 
+    /**
+     * Uploads all files and create folders in the remote server by using the <code>Uploader</code> class.
+     *
+     * @throws JSchException when an error occurs in <code>Uploader</code>
+     * @throws SftpException when an error occurs in <code>Uploader</code>
+     */
+
+    public void upload() throws JSchException, SftpException {
+        Uploader uploader = new Uploader(ipAddress, port, username, password, remotePath);
+        uploader.openConnection();
+        for(String i : relativeDir){
+            if(uploader.createFolder(i))
+                System.out.println("Created: " + i);
+        }
+
+        for (Path i : absoluteFile) {
+            String filePath = String.valueOf(i);
+            String serverPath = String.valueOf(remotePath.resolve(localPath.relativize(i)));
+            System.out.println("Uploading: " + filePath + " - to - " + serverPath);
+            uploader.uploadFile(filePath, serverPath);
+        }
+        uploader.close();
+    }
+
+
+    /**
+     * Relativizes a whole list of paths with <code>localPath</code>.
+     * @param absolutePath is a list of paths where only absolute version of them are kept
+     * @return a new list of relative paths
+     */
+
     public List<String> relativize(List<Path> absolutePath){
         List<String> temp = new ArrayList<>();
         absolutePath.forEach(x -> temp.add(String.valueOf(localPath.relativize(x))));
@@ -112,7 +166,10 @@ public class Client implements Updatable{
         return temp;
     }
 
-
+    /**
+     * Filters only directories out of all paths.
+     * @return a new list where only directories are kept
+     */
     public List<Path> directories(){
         List<Path> temp = new ArrayList<>();
         list.filter(Files::isDirectory).forEach(temp::add);
@@ -120,6 +177,10 @@ public class Client implements Updatable{
         return temp;
     }
 
+    /**
+     * Filters only files out of all paths, but does not include the checksum file for some reason.
+     * @return a new list where only files are kept
+     */
     public List<Path> files(){
         List<Path> temp = new ArrayList<>();
         list.filter(Files::isRegularFile).forEach(x -> {
@@ -131,14 +192,25 @@ public class Client implements Updatable{
         return temp;
     }
 
-
+    /**
+     *
+     * @param path is used to scan everything in it
+     * @return <code>Client</code>, so that it can be used with the method chaining to get files
+     * or directories
+     * @throws IOException when the given path could not be found
+     */
     public Client scanPath(Path path) throws IOException {
         list = Files.walk(path);
 
         return this;
     }
 
-    public String translator(char[] filePath){
+    /**
+     * Encodes the file path. Since URLs cannot contain spaces and other characters, they must be percent encoded.
+     * @param filePath char array of the path
+     * @return encoded path
+     */
+    public String URLEncoding(char[] filePath){
         StringBuilder t = new StringBuilder();
         for (char c : filePath) {
             if (c == ' ') t.append("%20");
--- src/main/java/com/zeynalovv/BI4L/Uploader.java
+++ src/main/java/com/zeynalovv/BI4L/Uploader.java
@@ -9,6 +9,14 @@ public final class Uploader {
     private Session jschSession = null;
     private final String ipAddress, port, username, password, remotePath;
 
+    /**
+     * A constructor for the class
+     * @param ipAddress IP address of the SFTP server
+     * @param port port for the connection
+     * @param username username in the SFTP server
+     * @param password password for the user
+     * @param remotePath the path where files are going to be stored
+     */
     public Uploader(String ipAddress, String port, String username, String password, Path remotePath) {
         this.ipAddress = ipAddress;
         this.port = port;
@@ -17,6 +25,10 @@ public final class Uploader {
         this.remotePath = String.valueOf(remotePath);
     }
 
+    /**
+     * Opens a tunnel between the server and <code>Uploader</code>
+     * @throws JSchException when an error occurs while opening a connection with the remote SFTP server
+     */
     public void openConnection() throws JSchException {
         JSch jsch = new JSch();
         jsch.setKnownHosts("~/.ssh/known_hosts");
@@ -28,10 +40,22 @@ public final class Uploader {
         channelSftp.connect();
     }
 
+    /**
+     * Transmits the file to the given path in the server.
+     * @param localFile file which is going to be transmitted
+     * @param remoteFile path where the file will go
+     * @throws SftpException when an error occurs while sending a data to the SFTP server
+     */
     public void uploadFile(String localFile, String remoteFile) throws SftpException {
         channelSftp.put(localFile, remoteFile);
     }
 
+    /**
+     * Creates directory in the server.
+     * @param directoryPath path for the creation
+     * @return status of the operation (if it is newly created, or it did exist)
+     * @throws SftpException when an error occurs while sending a command to the SFTP server
+     */
     public boolean createFolder(String directoryPath) throws SftpException {
         channelSftp.cd(remotePath);
         boolean check = false;
@@ -49,6 +73,9 @@ public final class Uploader {
         }
     }
 
+    /**
+     * Closes already opened connection.
+     */
     public void close(){
         jschSession.disconnect();
         channelSftp.exit();
